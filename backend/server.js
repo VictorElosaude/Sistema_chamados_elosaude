3ae011405682bcd675cc1ce917e65425bf158ba6
VictorElosaude <victor@elosaude.com.br>
2025-08-21 16:18:27 -0300

    Update server.js
    porta

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.use(cors());
